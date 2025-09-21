ce79ca48922405fea239040457000605ad1b0fb9
Andre Marcos Gelhen <gelhen@gmail.com>
2025-09-20 22:00:19 -0300

    API para consultar frequencia dos nomes e periodos de anos

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
 # asimov_apis
 Curso Asimov APIs com Python
+
+## link API IBGE
+https://servicodados.ibge.gov.br/api/docs/
+
+### Nomes de Pessoa
+https://servicodados.ibge.gov.br/api/docs/nomes?versao=2
+
+
---
+++ aula_10_acessando_primeira_api.py
@@ -0,0 +1,16 @@
+from pprint import pprint
+import requests
+
+nome = 'daniela'
+url = f"https://servicodados.ibge.gov.br/api/v2/censos/nomes/{nome}"
+resposta = requests.get(url=url)
+
+try:
+    resposta.raise_for_status()
+except requests.HTTPError as e:
+    print(f"Erro no request: {e}")
+    resultado = None
+else:
+    resultado = resposta.json()
+
+pprint(resultado)
