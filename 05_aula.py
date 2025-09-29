30c195e4c8ebc5bec1be7a4fa13c737f7efb134e
Andre Marcos Gelhen <gelhen@gmail.com>
2025-09-29 12:35:22 -0300

    Aula 05

--- 05_aula.py
+++ 05_aula.py
@@ -9,21 +9,29 @@ _ = load_dotenv(find_dotenv())
 #inicializa o client
 client = openai.Client()
 
+def geracao_texto(mensagens, model="gpt-3.5-turbo-0125", max_tokens=1000, temperature=0):
+    resposta = client.chat.completions.create(
+        messages=mensagens,
+        model=model,
+        max_tokens=max_tokens,
+        temperature=temperature,
+    )
+    print(resposta.choices[0].message.content)
+    print('consumo ', resposta.usage)
+    #mensagens.append({"role": "assistant", "content": resposta.choices[0].message.content})
+    # o mesmo que 
+    mensagens.append(resposta.choices[0].message.model_dump(exclude_none=True))
+    return mensagens
+
 #cria lista de dicts para enviar ao modelo
-messages = [
-    {"role": "user", "content": "O que é uma maçã em 50 palavras?"},
+mensagens = [
+    {"role": "user", "content": "O que é uma maçã em 5 palavras?"},
 ]
 #chama o modelo
-resposta = client.chat.completions.create(
-    messages=messages,
-    model="gpt-3.5-turbo-0125",
-    max_tokens=1000,
-    temperature=0,
-)
+mensagens = geracao_texto(mensagens=mensagens)
 
-pprint(resposta.choices[0].message.content)
+mensagens.append({"role": "user", "content": "E qual sua cor?"})
+#chama o modelo
+mensagens = geracao_texto(mensagens=mensagens)
 
-messages.append({"role": "assistant", "content": resposta.choices[0].message.content})
-
-pprint(messages)    
-messages.append({"role": "user", "content": "E qual sua cor?"})
+pprint(mensagens)
