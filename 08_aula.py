e849df9f56dc2f18275b62b57f8bbde3f4a72ef2
Andre Marcos Gelhen <andre.gelhen@tozzoalimentos.com.br>
2025-10-01 13:35:10 -0300

    Aula 8 Funções e ferramentas externas

---
+++ 08_aula.py
@@ -0,0 +1,100 @@
+import json
+
+import openai
+from dotenv import load_dotenv, find_dotenv
+
+_ = load_dotenv(find_dotenv())
+client = openai.Client()
+
+#1 - desenvolver a funcao para enviar ao modelo, resposta deve ser string
+#2 - criar o tools, descrevendo a funcao, campos da função que não tem valor default informar no campo required
+#3 - passa para o chat completion
+def obter_temperatura_atual(local, unidade="celsius"):
+    if "são paulo" in local.lower():
+        return json.dumps(
+            {"local": "São Paulo", "temperatura": "32", "unidade": unidade}
+            )
+    elif "porto alegre" in local.lower():
+        return json.dumps(
+            {"local": "Porto Alegre", "temperatura": "25", "unidade": unidade}
+            )
+    elif "rio de janeiro" in local.lower():
+        return json.dumps(
+            {"local": "Rio de Janeiro", "temperatura": "35", "unidade": unidade}
+            )
+    else:
+        return json.dumps(
+            {"local": local, "temperatura": "unknown"}
+            )
+
+
+
+tools = [
+    {
+        "type": "function",
+        "function": {
+            "name": "obter_temperatura_atual",
+            "description": "Obtém a temperatura atual em uma dada cidade",
+            "parameters": {
+                "type": "object",
+                "properties": {
+                    "local": {
+                        "type": "string",
+                        "description": "O nome da cidade. Ex: São Paulo",
+                    },
+                    "unidade": {
+                        "type": "string",
+                        "enum": ["celsius", "fahrenheit"]
+                    },
+                },
+                "required": ["local"],
+            },
+        },
+    }
+    ]
+# somente para facilitar a busca pela função
+funcoes_disponiveis = {
+        "obter_temperatura_atual": obter_temperatura_atual,
+    }
+# já passando uma mensage de busca
+mensagens = [
+    {"role": "user",
+     "content": "Qual é a temperatura em São Paulo e Porto Alegre?"}
+    ]
+
+resposta = client.chat.completions.create(
+    model="gpt-3.5-turbo-0125",
+    messages=mensagens,
+    tools=tools,
+    tool_choice="auto",
+)
+
+mensagem_resp = resposta.choices[0].message
+# me respondeu e avisa se precisa rodar alguma função
+tool_calls = mensagem_resp.tool_calls
+
+if tool_calls:
+    mensagens.append(mensagem_resp)
+    for tool_call in tool_calls:
+        function_name = tool_call.function.name
+        function_to_call = funcoes_disponiveis[function_name]
+        function_args = json.loads(tool_call.function.arguments)
+        function_response = function_to_call(
+            local=function_args.get("local"),
+            unidade=function_args.get("unidade"),
+        )
+        mensagens.append(
+            {
+                "tool_call_id": tool_call.id,
+                "role": "tool",
+                "name": function_name,
+                "content": function_response,
+            }
+        )
+    segunda_resposta = client.chat.completions.create(
+        model="gpt-3.5-turbo-0125",
+        messages=mensagens,
+    )
+
+mensagem_resp = segunda_resposta.choices[0].message
+print(mensagem_resp.content)
